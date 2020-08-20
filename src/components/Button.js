55b89c018cde1c9287104251e81cebdb06cb0074
Saalik Lokhandwala <saaliklok@pop-os.localdomain>
2020-08-20 08:09:11 -0400

    added mailchimp newsletter signup

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,14 +8,14 @@ export const Button = styled.a`
   border-radius: 100px;
   color: var(--accent-color);
   display: flex;
-  font-size: 26px;
+  font-size: 18px;
   font-weight: bold;
   justify-content: center;
-  margin: 10vh auto 5vh auto;
-  padding: 20px 50px;
+  margin: 2vh auto;
+  padding: 5px 20px;
   text-align: center;
   transition: box-shadow 0.3s cubic-bezier(0.23, 1, 0.32, 1);
-  width: 300px;
+  width: 200px;
 
   :hover {
     box-shadow: 0px 9px 0 0 var(--accent-color);
